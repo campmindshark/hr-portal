17fce71c06028db0fb265d489aba2501b6f99b16
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-12-28 16:39:32 -0600

    setup basic templating

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,10 +1,23 @@
 use rocket::{get, routes, Route};
+use rocket_contrib::templates::Template;
+use serde::Serialize;
 
 pub fn routes() -> Vec<Route> {
     routes![index]
 }
 
+#[derive(Serialize)]
+struct EmptyContext {
+    parent: &'static str,
+}
+
+impl Default for EmptyContext {
+    fn default() -> Self {
+        EmptyContext { parent: "layout" }
+    }
+}
+
 #[get("/")]
-pub fn index() -> &'static str {
-    "homepage"
+pub fn index() -> Template {
+    Template::render("index", &EmptyContext::default())
 }
